c2a22c7447888a4f7a5d312d59d4f61729178c5e
Juliabomb <82144073+Juliabomb@users.noreply.github.com>
2021-06-21 20:56:03 +0500

    Add files via upload

---
+++ Components/Body/Seurch/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Form } from 'react-bootstrap';
+
+export default function index (){
+    return (
+        <div>
+
+<Form.Control/>
+
+        </div>
+    )
+}
---
+++ Components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+
+export default function index (){
+    return (
+        <div>
+<h1 >Заголовок </h1>
+
+        </div>
+    )
+}

9287914d981f95ee8cab5f4aaaab943448c2dbc4
Juliabomb <82144073+Juliabomb@users.noreply.github.com>
2021-06-23 11:02:25 +0000

    Delete Components directory

--- Components/Body/Item/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function index (){
-    return (
-        <div>
-<h2>Item</h2>
-        </div>
-    )
-}
--- Components/Body/ItemList/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function index (){
-    return (
-        <div>
-<h3>ItemList</h3>
-        </div>
-    )
-}
